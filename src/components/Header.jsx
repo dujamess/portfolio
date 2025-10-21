e6ee1e90b89215a56e349a72f35e3c4433c96a43
Khadija Messah <khmessah@c4r10p10.1337.ma>
2025-10-21 14:04:26 +0100

    duva

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import { useState,useEffect } from "react";
 function Header () {
+    const [count,setCount] = useState(0);
+    useEffect(()=>console.log("new value of counter"),[count]);
     return (
         <div className="h-[46px] w-[747px] border-[0.5px] border-move1 rounded-[10px] flex justify-around pt-[4px]">
             <img className=" h-10 w-10" src="logo.gif" alt="logo"></img>
-            <button className="font-inter  text-hlibi text-[12px]">Home</button>
+            <button className="font-inter  text-hlibi text-[12px]" onClick={()=>{
+                setCount(count+1);
+                console.log("count is : ",count)
+            }}>Home</button>
             <button className="font-inter  text-hlibi text-[12px]">Projects</button>
             <button className="font-inter  text-hlibi text-[12px]">about-me</button>
         </div>
