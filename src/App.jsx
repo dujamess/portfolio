00487de374aed9634cddd66587d6906c94ac4650
dujamess <duja.dija.12@gmail.com>
2025-10-21 05:48:44 +0100

    duva

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Badge from './components/badg'
+import Skills from './components/skills'
 function App() {
 
   return (
-   <div>
+    <div className="flex flex-col items-center">
     <Badge/>
+    <Skills/>
    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='w-screen h-screen  flex justify-center items-start bg-move'>
+    <div className='w-screen  bg-move'>
       <div className="absolute w-full h-full bg-[url('anim-bg.gif')]  bg-[length:200px_200px] bg-repeat opacity-[0.30]"></div>
       <div className='relative w-full h-full flex flex-col justify-start pt-10 items-center '>
         <Header/>
         <App />
+      </div>
+      <div className='w-[150px] h-[150px] bg-amber-700 flex items-end justify-end'>
+        <img src="design-background.svg" alt='design-background' />
       </div>
     </div>
   </StrictMode>,
