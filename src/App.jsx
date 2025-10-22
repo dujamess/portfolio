7718412a7a05822e7ebf598b0f3bd9eb30760c74
Khadija Messah <khmessah@c4r1p10.1337.ma>
2025-10-22 11:39:58 +0100

    router all done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css'
 import Home from './components/Home/Home'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Projects from './components/Projects/Projects'
+import About from './components/AboutMe/AboutMe'
 function App() {
 
   return (
@@ -9,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home/>}/>
+          <Route path="/Projects" element={<Projects/>}/>
+          <Route path="/About-me" element={<About/>}/>
+          <Route path="*" element={<div>404 not found</div>}/>
         </Routes>
       </BrowserRouter>
    </div>
