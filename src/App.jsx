8b139ea740314534dd5233269f92816dfa0c5482
dujamess <duja.dija.12@gmail.com>
2025-10-25 19:19:17 +0100

    error page done

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,14 @@ function App() {
       <BrowserRouter>
         <div className='h-[46px] w-[747px] border-[0.5px] border-move1 rounded-[10px] justify-end  items-center flex space-x-24 pr-8'>
                 <img className=" w-10 mr-80"  src="logo.gif" alt="logo"></img>
-                <Link to="/home" className="font-inter  text-hlibi text-[12px]">Home</Link>
+                <Link to="/" className="font-inter  text-hlibi text-[12px]">Home</Link>
                 <Link to="/Projects" className="font-inter  text-hlibi text-[12px]">Projects</Link>
                 <Link to="/About-me" className="font-inter  text-hlibi text-[12px]">about-me</Link>
         </div>
         <div className="">
         </div>
         <Routes>
+          <Route path="/" element={<Home/>}/>
           <Route path="/home" element={<Home/>}/>
           <Route path="/Projects" element={<Projects/>}/>
           <Route path="/About-me" element={<About/>}/>
--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,17 @@
 export default function Error()
 {
     return (
-        <div className="mt-[200px]">
-            <p className="font-inter text-hlibi font-bold  text-[15px]">404 ! </p>
-            <p className="font-inter text-hlibi font-normal text-[10px]">Oops… The page you’re looking for doesn’t exist.</p>
+        <div>
+            <div className="flex-col space-y-2 mt-30">
+                <div className="flex justify-center">
+                    <p className="font-inter font-bold text-[15px] text-hlibi">404!</p>
+                </div>
+                <div className="flex">
+                        <img src="ops.svg" alt="ops"></img>
+                        <p className="font-inter font-normal text-[10px] text-hlibi">Oops… The page you’re looking for doesn’t exist.</p>
+                        <img src="ops1.svg" alt="ops"></img>
+                </div>
+            </div>
         </div>
     )
 }
