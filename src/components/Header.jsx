5cc359eb9da45c52034259ccd8e2375499425016
dujamess <duja.dija.12@gmail.com>
2025-10-26 12:18:34 +0100

    skills done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import { useState,useEffect } from "react";
+
 function Header () {
     return (
-        <div className="h-[46px] w-[747px] border-[0.5px] border-move1 rounded-[10px] flex justify-around pt-[4px]">
+        <div className=" w-[747px] border-[0.5px] border-move1 rounded-[10px] flex justify-around">
             <img className=" h-10 w-10" src="logo.gif" alt="logo"></img>
             <button className="font-inter  text-hlibi text-[12px]">Home</button>
             <button className="font-inter  text-hlibi text-[12px]">Projects</button>
--- src/components/Home/skills.jsx
+++ src/components/Home/skills.jsx
@@ -1,40 +1,40 @@
 function Skills()
 {
     return(
-        <div>
-            <div className="flex justify-center mt-24">
+        <div className="w-[747px]">
+            <div className="flex justify-center pt-24 pb-10">
                 <img src="Group.svg" alt=""></img>
                 <span className="font-inter text-[14px] text-hlibi">Skills</span>
             </div>
-            <div className="h-[46px] w-[747px] border-[0.5px] border-move1 bg-move rounded-[10px] flex justify-between pt-[4px] items-center mt-7 pl-2 pr-2">
-                <span className="font-inter  text-[#DEDED1] text-[12px]">Langague</span>
-                <span className="font-inter  text-[#DEDED1] text-[12px]">Frameworks</span>
-                <span className="font-inter  text-[#DEDED1] text-[12px]">Databases</span>
-                <span className="font-inter  text-[#DEDED1] text-[12px]">Tools</span>
-                <span className="font-inter  text-[#DEDED1] text-[12px]">Other</span>
+            <div className=" border-[0.5px] border-move1 bg-move rounded-[10px] flex pt-4 pb-4">
+                <span className="font-inter  text-[#DEDED1] text-[12px] w-[149px]  pl-10">Langague</span>
+                <span className="font-inter  text-[#DEDED1] text-[12px] w-[149px] pl-10">Frameworks</span>
+                <span className="font-inter  text-[#DEDED1] text-[12px] w-[149px] pl-11">Databases</span>
+                <span className="font-inter  text-[#DEDED1] text-[12px] w-[149px] pl-10">Tools</span>
+                <span className="font-inter  text-[#DEDED1] text-[12px] w-[149px] pl-10">Other</span>
             </div>
-            <div className="flex items-start justify-between mt-7 " >
-                <div className="bg-move  border-move1 font-normal rounded-[10px] text-[#C5C7BC] font-inter text-[11px] border-[0.5px] p-2  flex flex-col space-y-1">
+            <div className="flex  mt-7 space-x-4" >
+                <div className=" w-[149px] bg-move  border-move1 font-normal rounded-[10px] text-[#C5C7BC] font-inter text-[11px] border-[0.5px] flex flex-col p-3 space-y-2 ">
                     <p>-  Javascript</p>
                     <p>-  C</p>
                     <p>-  C++</p>
                 </div>
-                <div className="bg-move border-move1 rounded-[10px] text-[#C5C7BC] font-inter text-[11px] border-[0.5px] p-2 font-normal flex flex-col space-y-1 ">
+                <div className="w-[149px] bg-move border-move1 rounded-[10px] text-[#C5C7BC] font-inter text-[11px] border-[0.5px] p-3 font-normal flex flex-col space-y-2 ">
                     <p>-  React js</p>
                     <p>-  Fastify js</p>
                 </div>
-                <div className="bg-move border-move1 rounded-[10px] text-[#C5C7BC]  font-inter text-[11px] border-[0.5px] p-2 font-normal flex flex-col space-y-1">
+                <div className="bg-move border-move1 rounded-[10px] text-[#C5C7BC]  font-inter text-[11px] border-[0.5px] p-3 font-normal flex flex-col space-y-2 ">
                     <p>-  SQLite</p>
                     <p>-  MariaDB</p>
                     <p>-  PostgreSQL</p>
                 </div>
-                <div className="bg-move border-move1 rounded-[10px] text-[#C5C7BC]  font-inter text-[11px] border-[0.5px] p-2 font-normal flex flex-col space-y-1">
+                <div className="w-[149px] bg-move border-move1 rounded-[10px] text-[#C5C7BC]  font-inter text-[11px] border-[0.5px] p-3 font-normal flex flex-col space-y-2 ">
                     <p>-  Git</p>
                     <p>-  Github</p>
                     <p>-  Docker</p>
-                    <p>-  Docker Compose</p>
+                    <p>-  Docker</p>
                 </div>
-                <div className="bg-move border-move1 rounded-[10px] text-[#C5C7BC]  font-inter text-[11px] border-[0.5px] p-2 font-normal flex flex-col space-y-1">
+                <div className="w-[149px] bg-move border-move1 rounded-[10px] text-[#C5C7BC]  font-inter text-[11px] border-[0.5px] p-3 font-normal flex flex-col space-y-2 ">
                     <p>-  Html</p>
                     <p>-  css</p>
                     <p>-  Tailwind css</p>
