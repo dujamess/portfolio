0e41cccf4515ea23d4ef400572f8b9c3b9442ed4
dujamess <duja.dija.12@gmail.com>
2025-10-26 12:29:23 +0100

    font-extrabold

--- src/components/AboutMe/WorkExperience.jsx
+++ src/components/AboutMe/WorkExperience.jsx
@@ -19,13 +19,13 @@ export default function WorkExperience()
                     <div className="flex-col">
                         <br></br>
                         <div className="flex-col text-base/loose">
-                            <ul className="list-disc font-inter font-light text-hlibi text-[14px] pl-10">UM6P: Graphic Developer 3D 		</ul>
+                            <ul className="list-disc font-inter font-extrabold text-[#B6AE9F] text-[14px] pl-10">UM6P: Graphic Developer 3D 		</ul>
                             <li className="font-inter font-light text-hlibi text-[13px] ml-16">Modélisation d'objets 3D pour divers projets en utilisant Blender.  </li>
                             <p className="font-inter font-light text-hlibi text-[9px] ml-16">03/2022 – 06/2022</p>
                         </div>
                         <br></br>
                         <div className="flex-col text-base/loose">
-                            <ul className="list-disc font-inter font-light text-hlibi text-[14px] pl-10">EON Reality: Concepteur et Développeur d'Applications VR/AR</ul>
+                            <ul className="list-disc font-inter font-extrabold text-[#B6AE9F] text-[14px] pl-10">EON Reality: Concepteur et Développeur d'Applications VR/AR</ul>
                             <li className="font-inter font-light text-hlibi text-[13px] ml-16">Programmation d'interfaces utilisateur et développement de la
                 logique applicative pour des projets interactifs. </li>
                             <li className="font-inter font-light text-hlibi text-[13px] ml-16">Intégration d'assets 2D/3D et développement des interactions au sein du moteur Unity.</li>
