c0712a809d23f9778ed1ec656ab3f7b4965645f2
dujamess <duja.dija.12@gmail.com>
2025-10-27 11:44:44 +0100

    fixe error rendrening image

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,9 +8,9 @@ export default function Projects()
           </div>
           <div className="flex-col">
               <div className="flex justify-between mt-8">
-                  <div className="w-60 flex-col h-60 bg-move">
+                  <div className="w-60 flex-col h-55 bg-move">
                     <div className=" h-full border-[0.5px]  border-move1 rounded-t-[10px]">
-                      <img src="/gameconnecte.svg" alt="gameconnecte" className="w-60"></img>
+                      <img src="/gameconnecte.svg" alt="gameconnecte" className="w-60 h-[152px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">JavaScript,React,Fastify,Node.js,Microservices Architecture, HTML, CSS, Tailwind CSS</p>
                     </div>
                     <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
@@ -18,9 +18,9 @@ export default function Projects()
                       <a href="https://github.com/dujamess/GameConnect"  target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">GameConnect</a>
                     </div>
                   </div>
-                  <div className="w-60 flex-col h-60 bg-move">
+                  <div className="w-60 flex-col h-55 bg-move">
                     <div className=" h-full border-[0.5px] border-move1 rounded-t-[10px]">
-                      <img src="/UItrans.svg" alt="UItrans" className="w-60"></img>
+                      <img src="/UItrans.svg" alt="UItrans" className="w-60 h-[198px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">figma</p>
                     </div>
                       <div className=" bg-move2 border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
@@ -28,9 +28,9 @@ export default function Projects()
                         <a href="https://www.figma.com/design/0P8kPpsnqV9DJtwH1fThbQ/Untitled?node-id=402-3406&p=f&t=rHOELxtjnSQPAaiP-0" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">Ui-GameConnect</a>
                       </div>
                   </div>
-                <div className="w-60 flex-col h-60 bg-move">
+                <div className="w-60 flex-col h-55 bg-move">
                     <div className="h-full bg-move border-[0.5px] border-move1 rounded-t-[10px]">
-                      <img src="/corserve1.svg" alt="corserve" className="w-60"></img>
+                      <img src="/corserve1.svg" alt="corserve" className="w-60 h-[162px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">langag c++</p>
                       <br></br>
                     </div>
@@ -41,111 +41,111 @@ export default function Projects()
                   </div>
               </div>
 
-              {/* <div className="flex justify-around mt-8">
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
-                      <img src="/algoswap.svg" alt="algoswap"></img>
+              <div className="flex justify-between mt-8">
+                  <div className=" w-60 h-55 flex-col">
+                    <div className="h-full bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
+                      <img src="/algoswap.svg" alt="algoswap" className="w-60 h-[162px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">langague c</p>
                       <br></br>
                     </div>
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                    <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                       <img src="/icon-link.svg" alt="icon-link"></img>
                       <a href="https://github.com/dujamess/algoswap" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">AlgoSwap</a>
                     </div>
                   </div>
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move2 border-[0.5px] border-move1 rounded-t-[10px]">
-                      <img src="/portfolio.svg" alt="portfolio"></img>
+                  <div className="w-60 h-55 flex-col">
+                    <div className="h-full bg-move2 border-[0.5px] border-move1 rounded-t-[10px]">
+                      <img src="/portfolio.svg" alt="portfolio" className="w-60 h-[200px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">Javascript, react,tailwind css</p>
                     </div>
-                      <div className="w-[205px]  bg-move2 border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                      <div className=" bg-move2 border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                         <img src="/icon-link.svg" alt="icon-link"></img>
                         <a href="https://github.com/dujamess/portfolio" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">khadija-messah.me</a>
                       </div>
                   </div>
-                <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px] border-move1 rounded-t-[10px]">
-                      <img src="/folio.svg" alt="UIkhmessah"></img>
+                <div className="flex-col w-60 h-55">
+                    <div className="h-full bg-move border-[0.5px] border-move1 rounded-t-[10px]">
+                      <img src="/folio.svg" alt="UIkhmessah" className="w-60 h-[162px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">figma</p>
                       <br></br>
                     </div>
-                      <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                      <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                         <img src="/icon-link.svg" alt="icon-link" ></img>
                         <a href="https://www.figma.com/design/umEgYSSSezBEjrtsLUrGpE/Developer-Portfolio-Website-Design--Community-?node-id=0-1&p=f&t=rHOELxtjnSQPAaiP-0" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">UI-khadija-messah.me</a>
                       </div>
                   </div>
-              </div> */}
+              </div>
 
-              {/* <div className="flex justify-around mt-8">
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
-                      <img src="/Pseudo-3D.svg" alt="Pseudo-3D"></img>
+              <div className="flex justify-between mt-8">
+                  <div className="flex-col w-60 h-55">
+                    <div className="h-full bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
+                      <img src="/Pseudo-3D.svg" alt="Pseudo-3D" className="w-60 h-[159px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">langague c , ray-casting</p>
                       <br></br>
                     </div>
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                    <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                       <img src="/icon-link.svg" alt="icon-link"></img>
                       <a href="https://github.com/dujamess/Pseudo-3D" target="_blanck" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">Pseudo-3D</a>
                     </div>
                   </div>
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move2 border-[0.5px] border-move1 rounded-t-[10px]">
-                      <img src="/DockLab2.svg" alt="Docklab"></img>
+                  <div className="flex-col w-60 h-55">
+                    <div className="h-full bg-move2 border-[0.5px] border-move1 rounded-t-[10px]">
+                      <img src="/DockLab2.svg" alt="Docklab" className="w-60 h-[200px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">docker, docker-compose, virtuel machine</p>
                     </div>
-                      <div className="w-[205px]  bg-move2 border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                      <div className=" bg-move2 border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                         <img src="/icon-link.svg" alt="icon-link"></img>
                         <a href="https://github.com/dujamess/Docklab"  target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">Docklab</a>
                       </div>
                   </div>
-                <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px] border-move1 rounded-t-[10px]">
-                      <img src="/2DSprite.svg" alt="2DSprite"></img>
+                <div className="flex-col w-60 h-55">
+                    <div className="h-full  bg-move border-[0.5px] border-move1 rounded-t-[10px]">
+                      <img src="/2DSprite.svg" alt="2DSprite" className="w-60 h-[159px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">Langague c , bibliothèque graphique MiniLibX</p>
                       <br></br>
                     </div>
-                      <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                      <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                         <img src="/icon-link.svg" alt="icon-link"></img>
                         <a href="https://github.com/dujamess/2D-Sprite-Renderer" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">2D Sprite Renderer</a>
                       </div>
                   </div>
-              </div> */}
+              </div>
 
-              {/* <div className="flex justify-around mt-8">
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
-                      <img src="/smartcity.svg" alt="smartcity"></img>
+              <div className="flex justify-between mt-8">
+                  <div className="flex-col w-60 h-55">
+                    <div className="h-full  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
+                      <img src="/smartcity.svg" alt="smartcity" className="w-60 h-[159px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">C#, Unity, Blender,VR</p>
                       <br></br>
                     </div>
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                    <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                       <img src="/icon-link.svg" alt="icon-link"></img>
                       <a href="/Smart_City.mp4" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">SmartCity</a>
                     </div>
                   </div>
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
-                      <img src="/PDFCITY.svg" alt="PDFCITY"></img>
+                  <div className="flex-col w-60 h-55">
+                    <div className="h-full bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
+                      <img src="/PDFCITY.svg" alt="PDFCITY" className="w-60 h-[159px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">Blender</p>
                       <br></br>
                     </div>
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                    <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                       <img src="/icon-link.svg" alt="icon-link"></img>
                       <a href="/smarcity.pdf" target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">PDF-SmartCity</a>
                     </div>
                   </div>
-                  <div className="flex-col space-y-2">
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
-                      <img src="/Pink_room.svg" alt="Pink_room"></img>
+                  <div className="flex-col w-60 h-55">
+                    <div className="h-full  bg-move border-[0.5px]  border-move1 rounded-t-[10px]">
+                      <img src="/Pink_room.svg" alt="Pink_room" className="w-60 h-[159px]"></img>
                       <p className="font-inter font-light text-[9px] text-[#DEDED1] p-1">Blender</p>
                       <br></br>
                     </div>
-                    <div className="w-[205px]  bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
+                    <div className=" bg-move border-[0.5px]  border-move1 rounded-b-[10px] flex justify-center">
                       <img src="/icon-link.svg" alt="icon-link"></img>
                       <a href="/pink.mp4"  target="_blank" className="font-inter font-medium text-[12px] text-[#DEDED1] p-1">GamingRoom</a>
                     </div>
                   </div>
-              </div> */}
+              </div>
           </div>
       </div>
     )
